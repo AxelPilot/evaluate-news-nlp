bcf6f9414296a7bb53ddf74d805a551296a06268
Axel Smidt <axelpilot78@gmail.com>
2022-01-11 15:57:54 +0100

    Debugging

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,7 +3,7 @@ const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerWebpackPlugin = require('css-minimizer-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
-const WorkboxPlugin = require('workbox-webpack-plugin');
+// const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
     mode: 'production',
@@ -43,6 +43,6 @@ module.exports = {
         new MiniCssExtractPlugin({
             filename: '[name].css'
         }),
-        new WorkboxPlugin.GenerateSW()
+        // new WorkboxPlugin.GenerateSW()
     ]
 }
