34fe00ca45ac10eeabbc8c0d64f1bb22b01bfd83
Axel Smidt <axelpilot78@gmail.com>
2022-01-12 14:27:32 +0100

    Adds service worker

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
     mode: 'development',
@@ -30,6 +31,7 @@ module.exports = {
         new HtmlWebPackPlugin({
             template: './src/client/views/index.html',
             filename: './index.html'
-        })
+        }),
+        new WorkboxPlugin.GenerateSW()
     ]
 }
